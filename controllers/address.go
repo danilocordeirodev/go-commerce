bb132e2ed3ec3f1dc09e35a8a48af2bf31b070e7
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-04-27 12:07:46 -0300

    add delete address

--- controllers/address.go
+++ controllers/address.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
+	"github.com/danilocordeirodev/go-commerce/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func AddAddress() gin.HandlerFunc {
@@ -28,6 +33,22 @@ func DeleteAddress() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+
+		addressess := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addressess}}}}
+
+		UserCollection.UpdateOne(ctx, filter, update)
+	
 	}
 
 }
