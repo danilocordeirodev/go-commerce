1fc36a522b8c74bd707ea8855f7f060b9ed9ed04
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2023-05-08 11:40:35 -0300

    add middleware token

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,3 +1,31 @@
 package middleware
 
-func Authentication()
+import (
+	"net/http"
+
+	"github.com/danilocordeirodev/go-commerce/tokens"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization header provided"})
+			c.Abort()
+			return
+		}
+		
+		claims, err := tokens.ValidateToken(ClientToken)
+		if err == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+
+	}
+}
